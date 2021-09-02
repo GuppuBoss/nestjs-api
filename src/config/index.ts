19b8c581795e2da06aa7bd3dd9bf80d84b436c4a
abdullah <bsef19m506@pucit.edu.pk>
2021-09-02 16:12:48 +0500

    configuration added

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+const config = {
+  DATABASE_CONNECTION: 'DATABASE_CONNECTION',
+  DATABASE_CONNECTION_URL: 'mongodb://localhost:27017/abdullah',
+  USER_MODEL: 'USER_MODEL'
+
+}
+
+export default config;
