53d3ad09582e5b39d511a8f287628e134f840053
Danial munsif <bsef19m506@pucit.edu.pk>
2021-09-08 15:09:10 +0500

    forget password updation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
 import { UnauthorizedException } from "@nestjs/common";
-import { Controller, Get, Param, Body, Post, Res, Query } from "@nestjs/common";
+import { Controller, Get, Param, Body, Post, Res } from "@nestjs/common";
 import { Response } from "express";
 import config from "../config";
 import { ForgetPasswordService } from "../entities/forget-password";
@@ -24,20 +24,21 @@ export class UserController {
     @Param('token') token: string,
     @Res() res: Response
   ) {
-    console.log('came');
     jwt.verify(token, config.FORGET_PASSWORD_JWT_TOKEN_SECRET, (err, decodedEmail) => {
       if (err) throw new UnauthorizedException();
-      else res.redirect(`http://localhost:3000/forgetpassword?token=${token}`);
+      else res.redirect(`${config.FORGET_PASSWORD_REDIRECT_URL}${token}`);
     });
   }
-  @Post('forgetpass/recovery')
+  @Post('forgetpass/recovery/:token')
   UpdatePassword (
     @Body() body,
-    @Query() token?: string
+    @Param('token') token: string
   ) {
     !!token && jwt.verify(token, config.FORGET_PASSWORD_JWT_TOKEN_SECRET, async (err, decodedEmail) => {
-      if (err) throw new UnauthorizedException();
-      else { 
+      if (err) {
+        throw new UnauthorizedException();
+      }
+      else {
         const user = await this.userService.findOneByEmail(decodedEmail.email);
         user.password = body.password;
         await user.save()
